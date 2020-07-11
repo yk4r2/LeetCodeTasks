0f6831ae8565a98328f25b86a42e37fc8cacb43d
yk4r2 <zhestov.ea@phystech.edu>
2020-07-12 02:40:33 +0300

    more elegant solution of the 1st task

--- 1. Two Sum/main.cpp
+++ 1. Two Sum/main.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    vector<int> twoSum(vector <int>& nums, int target) {
-       map<int, int> st;
-            int i, n = nums.size();
-            vector<int> found;
-            for (i = 0; i < n; i++)
-            {
-                // int pos=;
-                map<int, int>::iterator it;
-                it = st.find(target - nums[i]);
-                if (it != st.end())
-                {
-
-                    found.push_back(it->second);
-
-                    found.push_back(i);
-                    break;
-                }
-                else
-                {
-                    st.insert({nums[i], i});
-                }
-            }
-            return found;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> prevMap;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            
+            if(prevMap.count(diff)) 
+                return { prevMap[diff], i };
+            prevMap[nums[i]] = i;
+        }
+        return {};
     }
 };
