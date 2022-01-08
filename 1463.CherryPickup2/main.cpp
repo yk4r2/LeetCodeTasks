2999c2f7cc711cedeb0fb51dc49bba932ec4bab3
yk4r2 <zhestov.ea@phystech.edu>
2022-01-09 00:34:52 +0300

    CPPLINT: daily Hard Matrix DP lint passed

--- 1463.CherryPickup2/main.cpp
+++ 1463.CherryPickup2/main.cpp
@@ -7,10 +7,10 @@ using std::cout, std::endl, std::vector, std::max;
 
 class Solution {
  public:
-  static int cherryPickup(vector<vector<int>>& grid) {
+  static int cherryPickup(vector<vector<int>> grid) {
   // n :: rows count, m :: columns count in the grid
-  int m = (int) grid.size();
-  int n = (int) grid[0].size();
+  int m = static_cast<int>(grid.size());
+  int n = static_cast<int>(grid[0].size());
   // memo :: DP caching array (size chosen due to constraints)
   int memo[70][70][70] = {-1};
 
@@ -20,7 +20,8 @@ class Solution {
     for (int col2 = 0; col2 < n; ++col2)
       // Little speed-up from no-branch programming
       // + (condition) * (smth) means we add (smth) if condition is met
-      memo[row][col1][col2] = grid[row][col1] + (col1 != col2) * grid[row][col2];
+      memo[row][col1][col2] = grid[row][col1] +
+        (col1 != col2) * grid[row][col2];
 
   // First let us iterate through all the rows bottom >> up
   // Robot 1 (col1): left >> right, Robot 2 (col2): right >> left;
@@ -29,7 +30,8 @@ class Solution {
     for (int col2 = 0; col2 < n; ++col2) {
       // Little speed-up from no-branch programming
       // + (condition) * (smth) means we add (smth) if condition is met
-      memo[row][col1][col2] = grid[row][col1] + (col1 != col2) * grid[row][col2];
+      memo[row][col1][col2] = grid[row][col1] +
+        (col1 != col2) * grid[row][col2];
       int max_cherries = 0;
       // Robot could come to current (row, col1, col2) only from
       // (row - 1, col1 ± 1, col2 ± 1) due to constraints
@@ -46,6 +48,10 @@ class Solution {
 
 
 int main() {
-  vector<vector<int>> grid = {{1,0,0,0,0,0,1},{2,0,0,0,0,3,0},{2,0,9,0,0,0,0},{0,3,0,5,4,0,0},{1,0,2,3,0,0,6}};
+  vector<vector<int>> grid = {{1, 0, 0, 0, 0, 0, 1},
+                {2, 0, 0, 0, 0, 3, 0},
+                {2, 0, 9, 0, 0, 0, 0},
+                {0, 3, 0, 5, 4, 0, 0},
+                {1, 0, 2, 3, 0, 0, 6}};
   cout << Solution::cherryPickup(grid);
 }
