eefccad521f2edac6aae33c6c2d8e28c49ee92dd
yk4r2 <zhestov.ea@phystech.edu>
2022-01-12 23:15:56 +0300

    SOLUTION: Medium DP Matrix, lint passed

--- 931.MinimumFallingPathSum/main.cpp
+++ 931.MinimumFallingPathSum/main.cpp
@@ -7,7 +7,7 @@ using std::cout, std::max, std::endl, std::vector, std::min;
 
 class Solution {
  public:
-  int minFallingPathSum(vector<vector<int>>& matrix) {
+  int minFallingPathSum(vector<vector<int>> matrix) {
   int n = matrix.size();
   if (n == 1)
     return matrix[0][0];
@@ -18,7 +18,9 @@ class Solution {
     // right-side (no n element here)
     matrix[i][n - 1] += min(matrix[i + 1][n - 1], matrix[i + 1][n - 2]);
     for (int j = 1; j < n - 1; ++j) {
-    matrix[i][j] += min(min(matrix[i + 1][j], matrix[i + 1][j - 1]), matrix[i + 1][j + 1]);
+    matrix[i][j] += min(
+      min(matrix[i + 1][j], matrix[i + 1][j - 1]),
+      matrix[i + 1][j + 1]);
     }
   }
   return *min_element(matrix[0].begin(), matrix[0].end());
