39ce3ef56601d6d14487d05a81b09ef2603cfc2e
yk4r2 <zhestov.ea@phystech.edu>
2022-02-06 21:18:44 +0300

    SOLUTION: Medium Hash Table Sliding Window

---
+++ 904.FruitIntoBaskets/main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        n = len(fruits)
+        if len(Counter(fruits)) < 3:
+            return n
+
+        dic = defaultdict(int)
+        res, st = 0, 0
+        for end in range(n):
+            dic[fruits[end]] += 1
+            while len(dic) > 2:
+                dic[fruits[st]] -= 1
+                if dic[fruits[st]] == 0:
+                    del dic[fruits[st]]
+                st += 1
+            res = max(res, end - st + 1)
+        return max(res, n - st)
