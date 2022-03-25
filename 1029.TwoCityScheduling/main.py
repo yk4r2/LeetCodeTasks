82c0dd26ad9af45fb21982ed2c18f9bde9517675
yk4r2 <zhestov.ea@phystech.edu>
2022-03-25 21:49:07 +0300

    SOLUTION: daily Medium Array Greedy Sorting

--- 1029.TwoCityScheduling/main.py
+++ 1029.TwoCityScheduling/main.py
@@ -1,8 +1,5 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         costs = sorted(costs, key=lambda x: x[0] - x[1])
-        total = 0
-        size = len(costs) // 2
-        for i in range(size):
-            total += costs[i][0] + costs[i + size][1]
-        return total
+        total, size = 0, len(costs) // 2
+        return reduce(lambda a, x: a + x[0][0] + x[1][1], zip(costs[:size], costs[size:]), 0)
