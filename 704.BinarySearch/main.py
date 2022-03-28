ec6327c07afb0cd274003ea2ae3675c28d0693ee
yk4r2 <zhestov.ea@phystech.edu>
2022-03-28 21:48:50 +0300

    SOLUTION: Easy Array Binary Search

---
+++ 704.BinarySearch/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
